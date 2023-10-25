82af2e04a896dfa768048ce0cfc48f74bd8d6129
Dung Tran <antalois23@gmail.com>
2023-10-25 21:08:46 +0700

    complete auth routes

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+const MainLayout = async ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return ( 
+    <div className="h-full">
+      
+      <main className="md:pl-[72px] h-full">
+        {children}
+      </main>
+    </div>
+   );
+}
+ 
+export default MainLayout;
