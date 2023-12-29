8e9beaa2c2a8ec05e4e09ee8cb8cafe31bc46959
Dung Tran <antalois23@gmail.com>
2023-12-29 22:03:38 +0700

    feat: forget password

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import { Profile } from '@prisma/client';
 import { db } from '@/prisma/db';
 import { UUID, randomUUID } from 'node:crypto';
+import nodemailer from 'nodemailer';
 
 export const getIp = (req: Request) =>
   (req.headers['x-forwarded-for'] || req.socket.remoteAddress)?.toString();
@@ -30,3 +31,28 @@ export const getFormatedDate = () => {
   const now = new Date();
   return `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
 };
+
+export const sendPasswordResetEmail = (email: string, token: string) => {
+  const transporter = nodemailer.createTransport({
+    // Your email configuration goes here
+  });
+
+  const resetLink = `http://localhost:8000/api/auth/reset?token=${token}`;
+
+  const mailOptions = {
+    from: 'your-email@example.com',
+    to: email,
+    subject: 'Password Reset',
+    text: `Click the following link to reset your password: ${resetLink}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+};
+
+
