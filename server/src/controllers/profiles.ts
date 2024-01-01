29a358d594d4cfff636686999fd451c42b946c81
Dung Tran <antalois23@gmail.com>
2024-01-01 09:57:09 +0700

    feat: add profile route

--- server/src/controllers/profiles.ts
+++ server/src/controllers/profiles.ts
@@ -6,6 +6,7 @@ import { createMsg, uuid } from '@/lib/utils';
 import path from 'path';
 import fsPromises from 'fs/promises';
 import fs from 'fs';
+import bcrypt from 'bcrypt';
 import { AuthenticatedRequest } from '@/lib/types';
 
 export const getProfileById = async (req: Request, res: Response) => {
@@ -88,14 +89,115 @@ export const uploadProfileImage = async (
       .webp()
       .toFile(path.join(absImageFolderPath, imageName));
 
-    await db.profile.update({
+    const updatedProfile = await db.profile.update({
       where: { id: profileId },
       data: {
         imageUrl: relImagePath,
       },
     });
 
-    return res.status(200).json(profile);
+    return res.status(200).json(updatedProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(
+      createMsg({
+        type: 'error',
+      }),
+    );
+  }
+};
+
+export const changeProfileImage = async (
+  req: AuthenticatedRequest<any, any, any>,
+  res: Response,
+) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: 'Require profile image' });
+    }
+
+    const profileId = req.user?.profileId!;
+    const profile = await db.profile.findUnique({
+      where: { id: profileId },
+    });
+
+    if (!profile) {
+      return res.status(400).json({ message: 'Profile not found' });
+    }
+
+    const image = req.file;
+    const folderPath = '/public/user';
+    const imageName = `${uuid()}.webp`;
+    const relImagePath = path.join(folderPath, imageName);
+    const absImageFolderPath = path.join(__dirname, '..', '..', folderPath);
+
+    if (!fs.existsSync(absImageFolderPath)) {
+      await fsPromises.mkdir(absImageFolderPath, {
+        recursive: true,
+      });
+    }
+
+    await sharp(image.buffer)
+      .resize(300, 300)
+      .webp()
+      .toFile(path.join(absImageFolderPath, imageName));
+
+    if (profile.imageUrl) {
+      const existingImagePath = path.join(__dirname, '..', '..', profile.imageUrl);
+
+      if (fs.existsSync(existingImagePath)) {
+        await fsPromises.unlink(existingImagePath);
+      }
+    }
+
+    const updatedProfile = await db.profile.update({
+      where: { id: profileId },
+      data: {
+        imageUrl: relImagePath,
+      },
+    });
+
+    return res.status(200).json(updatedProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(
+      createMsg({
+        type: 'error',
+      }),
+    );
+  }
+};
+
+export const changePassword = async (req: AuthenticatedRequest<any, any, any>, res: Response) => {
+  try {
+    const profileId = req.user?.profileId!;
+    const profile = await db.profile.findUnique({
+      where: { id: profileId },
+    });
+
+    if (!profile) {
+      return res.status(400).json({ message: 'Profile not found' });
+    }
+    const { password } = req.body;
+
+    if (!password) {
+      return res.status(400).json(
+        createMsg({
+          type: 'invalid',
+          invalidMessage: 'Need new password',
+        }),
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.profile.update({
+      where: { id: profileId },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(200).json({ message: 'Change password successful' });
   } catch (error) {
     console.error(error);
     return res.status(500).json(
--- server/src/routes/profiles.ts
+++ server/src/routes/profiles.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { getProfileById, uploadProfileImage } from '@/controllers/profiles';
+import { getProfileById, uploadProfileImage, changeProfileImage, changePassword } from '@/controllers/profiles';
 
 const upload = multer({ dest: 'public/user' });
 const profilesRouter = Router();
 
 profilesRouter.get('/:id', getProfileById);
-profilesRouter.post('/upload/:id', upload.single('image'), uploadProfileImage);
+profilesRouter.post('/upload', upload.single('image'), uploadProfileImage);
+profilesRouter.put('/upload', upload.single('image'), changeProfileImage);
+profilesRouter.get('/:id', getProfileById);
+profilesRouter.post('/changePassword', changePassword);
+
 
 export default profilesRouter;
