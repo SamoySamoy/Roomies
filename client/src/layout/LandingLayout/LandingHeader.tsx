16a7c09109a3eba9618a20f0f24c7d0756f44ba1
lqnquang <21020381@vnu.edu.vn>
2024-01-02 23:18:06 +0700

    edit: fix missing punctuation error

--- client/src/layout/LandingLayout/LandingHeader.tsx
+++ client/src/layout/LandingLayout/LandingHeader.tsx
@@ -60,30 +60,32 @@ const LandingHeader = () => {
           <>
             <div className='h-8 border-l border-gray-400/30'></div>
 
-        <ActionTooltip label='Login to connect with your friends'>
-          <Link
-            to={'/login'}
-            className='transition duration-300 ease-in-out transform hover:scale-110 hover:underline rounded-full'
-          >
-            <Button
-              variant='outline'
-              className='text-white border border-zinc-600 shadow-[0_0_2rem_-0.5rem_#fff7] font-bold rounded-full hover:border-emerald-500 hover:text-emerald-500 bg-zinc-900 hover:bg-zinc-900 w-24'
-            >
-              Sign In
-            </Button>
-          </Link>
-        </ActionTooltip>
+            <ActionTooltip label='Login to connect with your friends'>
+              <Link
+                to={'/login'}
+                className='transition duration-300 ease-in-out transform hover:scale-110 hover:underline rounded-full'
+              >
+                <Button
+                  variant='outline'
+                  className='text-white border border-zinc-600 shadow-[0_0_2rem_-0.5rem_#fff7] font-bold rounded-full hover:border-emerald-500 hover:text-emerald-500 bg-zinc-900 hover:bg-zinc-900 w-24'
+                >
+                  Sign In
+                </Button>
+              </Link>
+            </ActionTooltip>
 
-        <ActionTooltip label='Create a new account and discover more'>
-          <Link
-            to={'/register'}
-            className='rounded-full transition duration-300 ease-in-out transform hover:scale-110 hover:underline dark:hover:shadow-[0_0_2rem_-0.5rem_#fff]'
-          >
-            <Button className='bg-emerald-500 rounded-full w-24'>
-              <span className='text-black font-bold text-background '>Get started</span>
-            </Button>
-          </Link>
-        </ActionTooltip>
+            <ActionTooltip label='Create a new account and discover more'>
+              <Link
+                to={'/register'}
+                className='rounded-full transition duration-300 ease-in-out transform hover:scale-110 hover:underline dark:hover:shadow-[0_0_2rem_-0.5rem_#fff]'
+              >
+                <Button className='bg-emerald-500 rounded-full w-24'>
+                  <span className='text-black font-bold text-background '>Get started</span>
+                </Button>
+              </Link>
+            </ActionTooltip>
+          </>
+        )}
       </div>
     </div>
   );
