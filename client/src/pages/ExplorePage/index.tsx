e7f2952e22823ffd17a4755ae4ee1b0fb1f18284
atsayu <quyenhoangntt03@gmail.com>
2024-01-06 18:53:09 +0700

    Fix search in explore page

--- client/src/pages/ExplorePage/index.tsx
+++ client/src/pages/ExplorePage/index.tsx
@@ -3,6 +3,7 @@ import Featured from './Featured';
 import Search from './Search';
 import { LoadingPage } from '@/components/Loading';
 import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const ExplorePage = () => {
   const {
@@ -23,7 +24,19 @@ const ExplorePage = () => {
       refetchOnMount: true,
     },
   );
+  
+  const [query, setQuery] = useState('');
+  const filterdRoom = rooms?.filter(room => {
+    if (query.length === 0) return true;
+    return room.name.split(' ').some(word => {
+      return word.toLowerCase().startsWith(query);
+    });
+  });
 
+  function handleChane(e: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
+  }
+  
   if (isPending || isFetching) {
     return <LoadingPage />;
   }
@@ -34,8 +47,8 @@ const ExplorePage = () => {
 
   return (
     <div className='bg-white dark:bg-[#313338] w-full min-h-full px-4 py-2'>
-      <Search />
-      <Featured rooms={rooms} />
+      <Search query={query} onChange={handleChane} />
+      <Featured rooms={filterdRoom} />
     </div>
   );
 };
